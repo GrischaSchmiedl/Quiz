9b82c23663a97c90da862d598b34a1ceb8ebad5f
Grischa Schmiedl <grischa.schmiedl@fhstp.ac.at>
2025-03-10 19:48:48 +0100

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'question-list',
+    loadComponent: () => import('./pages/question-list/question-list.page').then( m => m.QuestionListPage)
   },
 ];
